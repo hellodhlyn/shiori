f40cce4b6c5e9acfc996fdc49562584b6bfc4630
Hoerin Doh <hellodhlyn@gmail.com>
2023-02-04 22:57:44 +0900

    Add a post field to viewer type

--- app/graphql/types/viewer_type.rb
+++ app/graphql/types/viewer_type.rb
@@ -4,7 +4,23 @@ class Types::ViewerType < Types::Base::Object
   field :email,             String, null: false
   field :profile_image_url, String
 
+  field :post, Types::PostType do
+    argument :site, String, required: false
+    argument :namespace, String, required: false
+    argument :slug, String, required: false
+    argument :uuid, String, required: false
+    validates required: { one_of: [[:site, :namespace, :slug], :uuid] }
+  end
+
   field :posts, Types::PostType.connection_type
+
+  def post(site: nil, namespace: nil, slug: nil, uuid: nil)
+    return Post.with_private.find_by(uuid: uuid, author: viewer) if uuid.present?
+
+    site      = Site.find_by(slug: site) or return nil
+    namespace = Namespace.find_by(site: site, slug: namespace) or return nil
+    Post.with_private.find_by(namespace: namespace, slug: slug, author: viewer)
+  end
 
   def posts
     viewer.posts.with_private

9cceabc1a45f91d79bf947c96ab76347a6dd3b8f
Hoerin Doh <hellodhlyn@gmail.com>
2024-09-16 14:29:37 +0900

    Upgrade dependencies and fix vulnerabilities

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,7 @@
 # SimpleCov should be loaded before any of application code
 require "simplecov"
-require "simplecov-lcov"
 
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::LcovFormatter,
-])
-
+SimpleCov.formatters = SimpleCov::Formatter::HTMLFormatter
 SimpleCov.start "rails" do
   add_filter "/spec/"
 end
